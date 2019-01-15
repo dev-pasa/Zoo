9ee1b3ce644e373ef837563972ed43ea8fcaffc1
dev-pasa <dev5@uw.edu>
2019-01-15 12:54:56 -0800

    clean code

--- Zoo/Classes/Cats.cs
+++ Zoo/Classes/Cats.cs
@@ -6,22 +6,8 @@ using System.Text;
 namespace Zoo.Classes
 {
     public abstract class Cats : Mammal
-    {
-
-        public override int Age { get; set; } = 10;
-
-        public override string Name { get; set; } = "Cat Family";
-        
+    {             
         public virtual int ClawLength { get; set; }
-
-        public override string Food()
-        {
-            return "Cat Food";
-        }
-        public override string Sound()
-        {
-            return "Meoww!!";
-        }
 
         //abstract method
         public abstract string Hunts();
